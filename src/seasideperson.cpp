3445b7961b65b065c8580b8a262aaf6a40543c65
Matt Vogt <matthew.vogt@jollamobile.com>
2013-05-08 14:07:04 +1000

    [nemo-qml-plugins] Use the correct name for the IconPath property

--- src/seasideperson.cpp
+++ src/seasideperson.cpp
@@ -869,7 +869,7 @@ QStringList SeasidePerson::accountIconPaths() const
     foreach (const QContactOnlineAccount &account, mContact.details<QContactOnlineAccount>()) {
         // Include the icon path value for each account returned by accountPaths
         if (account.hasValue("AccountPath")) {
-            rv.append(account.value("ServiceIconPath"));
+            rv.append(account.value("AccountIconPath"));
         }
     }
 
@@ -882,7 +882,7 @@ void SeasidePerson::addAccount(const QString &path, const QString &uri, const QS
     detail.setValue("AccountPath", path);
     detail.setAccountUri(uri);
     detail.setServiceProvider(provider);
-    detail.setValue("ServiceIconPath", iconPath);
+    detail.setValue("AccountIconPath", iconPath);
 
     mContact.saveDetail(&detail);
 
